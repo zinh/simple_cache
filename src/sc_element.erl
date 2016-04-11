1f5ad64c871d0aae1c195d47820827dba4556291
Vinh Nguyen <xuanvinhct@gmail.com>
2016-04-11 13:38:10 +0700

    init project

---
+++ src/sc_element.erl
@@ -0,0 +1,20 @@
+-module(sc_element).
+-behaviour(gen_server).
+
+-define(DEFAULT_LEASE_TIME, (60*60*25)).
+-define(SERVER, ?MODULE).
+
+-record(state, {value, lease_time, start_time}).
+
+start_link(Value, LeaseTime) ->
+  gen_server:start_link(?SERVER, [Value, LeaseTime], []).
+
+init([Value, LeaseTime]) ->
+  StartTime = calendar:datetime_to_gregorian_seconds(Now),
+  {ok, state#{value=Value,
+              lease_time = LeaseTime,
+              start_time = StartTime},
+   time_left(LeaseTime, StartTime)
+  }.
+
+time_left(LeaseTime, StartTime) ->
---
+++ src/sc_store.erl
@@ -0,0 +1,1 @@
+-module(sc_store).
